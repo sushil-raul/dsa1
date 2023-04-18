de2adb861f720595b0eb443cc30610d03f8f09d2
sushil-raul <125520931+sushil-raul@users.noreply.github.com>
2023-04-18 07:16:02 +0530

    Update qha2.java

---
+++ src/qha2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class HW2
+
+{
+
+public static void main(String[] args)
+
+Scanner obj=new Scanner(System.in);
+
+System.out.print("Enter the number of lines for input: "); int n-obj.nextInt();
+
+obj.nextLine();
+
+String[] s=new String[n]; System.out.println("Enter lines: ") ;
+
+for(int i=0;i<n;i++)
+
+s[1]-obj.nextLine();
+
+System.out.println("Lines in reverse ordered are : "); for(int j=n-1;j>=0;j--) System.out.println(s[j]);
+
+obj.close();
+}
+}
